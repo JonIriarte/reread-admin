a7428ecf19f082e4548a3a823f903353c130f909
Jon Iriarte <joniriartesulleiro@gmail.com>
2020-10-19 20:02:40 +0200

    Subida con lo hecho en clase que funciona

--- controller/loginController.php
+++ controller/loginController.php
@@ -4,11 +4,11 @@ include '../model/userDAO.php';
 if (isset($_POST['email'])) {
     $user = new User($_POST['email'], md5($_POST['psswd']));
     $userDAO = new UserDAO();
-    if($userDAO->login($user->getEmail(),$user->getPasswd())){
+    if($userDAO->login($user)){
         echo 'perfect';
         // establecer sesiones
-        // redirección a ebook.admin.php
-    }else {
+        header('Location:../view/zona.admin.php'); 
+    }else{
         header('Location: ../view/login.php');
     }
 }else {
--- model/userDAO.php
+++ model/userDAO.php
@@ -1,21 +1,32 @@
 <?php
+
+//require_once './user.php';
+//DAO=Data Access Object. En los DAO se hacen las querys. 
 class UserDao{
+    //PDO=PHP Data Objects. 
     private $pdo;
 
     public function __construct(){
         include 'connection.php';
         $this->pdo=$pdo;
     }
-
-    public function login($email, $psswd){
-        $query = "SELECT * FROM tbl_user WHERE email=? AND passwd=?";
+    public function login($user){
+        $query = "SELECT * FROM users WHERE Email=? AND Pass=?";
         $sentencia=$this->pdo->prepare($query);
+        $email=$user->getEmail(); 
+        $psswd=$user->getPassword(); 
         $sentencia->bindParam(1,$email);
         $sentencia->bindParam(2,$psswd);
         $sentencia->execute();
         $result=$sentencia->fetch(PDO::FETCH_ASSOC);
         $numRow=$sentencia->rowCount();
         if(!empty($numRow) && $numRow==1){
+            $user->setName($result['Name']);
+            $user->setId_user($result['Id']);  
+            //Creamos sesión
+            session_start(); 
+            //Se le asigna a
+            $_SESSION['user']=$user;  
             return true;
         }else {
             return false;
